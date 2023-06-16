53632495cdcc6968d7dd6f8be046b7c6c0e32481
Manuu <125753742+Manusm2712@users.noreply.github.com>
2023-06-15 21:00:17 -0500

    Add files via upload

---
+++ Programa_notas/src/main/java/com/mycompany/programa_notas/Programa_notas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa_notas;
+
+/**
+ *
+ * @author maria
+ */
+public class Programa_notas {
+
+    public static void main(String[] args) {
+        Ventana miVentanaPrincipal = new Ventana ();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
+
